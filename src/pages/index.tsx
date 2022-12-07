dcbd0ec21703ffcb8d660b3819eb6befcee35549
ElliotLuque <elliotluque@gmail.com>
2022-12-07 02:18:51 +0100

    middleware test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,73 +1,80 @@
-import { type NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
+import { signIn, signOut } from "next-auth/react";
+import { getServerAuthSession } from "../server/common/get-server-auth-session";
 import Link from "next/link";
-import { signIn, signOut, useSession } from "next-auth/react";
 
-import { trpc } from "../utils/trpc";
-
-const Home: NextPage = () => {
+const LandingPage: NextPage = () => {
   return (
-    <>
+    <div>
+      <header>
+        <nav className="flex flex-wrap items-center justify-between bg-gradient-to-b from-[#15162c] to-[#2e026d] p-6">
+          <Link
+            className="mr-6 flex flex-shrink-0 flex-row items-center text-white"
+            href="/"
+          >
+            Logo
+          </Link>
+          <div>
+            <ul>
+              <li className="mr-4 inline-block">
+                <Link
+                  className="mt-4 mr-4 block text-white hover:text-white lg:mt-0 lg:inline-block"
+                  href="/"
+                >
+                  Item 1
+                </Link>
+              </li>
+              <li className="mr-4 inline-block">
+                <Link
+                  className="mt-4 mr-4 block text-white hover:text-white lg:mt-0 lg:inline-block"
+                  href="/"
+                >
+                  Item 2
+                </Link>
+              </li>
+              <li className="mr-4 inline-block">
+                <Link
+                  className="mt-4 mr-4 block text-white hover:text-white lg:mt-0 lg:inline-block"
+                  href="/"
+                >
+                  Item 3
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <button
+            className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+            onClick={() => signIn("auth0")}
+          >
+            Sign in
+          </button>
+        </nav>
+      </header>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-          </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
-            >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
-            >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
-          </div>
-          <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              Cositas
-            </p>
-            <AuthShowcase />
-          </div>
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-5xl font-bold text-white">Landing Page TO-DO</h1>
         </div>
       </main>
-    </>
+    </div>
   );
 };
 
-export default Home;
+export default LandingPage;
 
-const AuthShowcase: React.FC = () => {
-  const { data: sessionData } = useSession();
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { req, res } = context;
+  const session = await getServerAuthSession({ req, res });
 
-  const { data: secretMessage } = trpc.auth.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined },
-  );
+  if (session) {
+    return {
+      redirect: {
+        destination: "/dashboard",
+        permanent: false,
+      },
+    };
+  }
 
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => signOut() : () => signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
-  );
+  return {
+    props: { session },
+  };
 };
