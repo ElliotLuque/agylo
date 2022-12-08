6073185ea679f2ca63e5fe401b7da1b8f18efae1
ElliotLuque <elliotluque@gmail.com>
2022-12-08 01:37:25 +0100

    add protected page reusable function

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,16 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
+import { signOut } from "next-auth/react";
+import { protectedPage } from "../server/common/protected-page";
 
 const Dashboard: NextPage = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      Dashboard
+      <button onClick={() => signOut()}>sign out</button>
+    </div>
+  );
 };
 
 export default Dashboard;
+
+export const getServerSideProps: GetServerSideProps = async (context) => protectedPage(context)
---
+++ src/server/common/protected-page.ts
@@ -0,0 +1,20 @@
+import type { GetServerSideProps } from "next";
+import { getServerAuthSession } from "./get-server-auth-session";
+
+export const protectedPage: GetServerSideProps = async (context) => {
+    const { req, res } = context;
+    const session = await getServerAuthSession({ req, res });
+  
+    if (!session) {
+      return {
+        redirect: {
+          destination: "/",
+          permanent: false,
+        },
+      };
+    }
+  
+    return {
+      props: { session },
+    };
+  };
