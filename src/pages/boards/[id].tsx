0de1f755761880113ce2ca3e54c86708fc4eac29
ElliotLuque <elliotluque@gmail.com>
2022-12-09 01:22:14 +0100

    add placeholder board page

---
+++ src/pages/boards/[id].tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import Header from "../../components/dashboard/header";
+
+const KanbanLayout: NextPage = () => {
+  return (
+    <div>
+      <Header />
+      Kanban Layout
+    </div>
+  );
+};
+export default KanbanLayout;
