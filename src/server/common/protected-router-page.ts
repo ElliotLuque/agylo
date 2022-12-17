e3a5f7b5503ce33028d2c1545f5192718b01a455
ElliotLuque <elliotluque@gmail.com>
2022-12-17 14:51:33 +0100

    lint protectedRouterPage

--- src/server/common/protected-router-page.ts
+++ src/server/common/protected-router-page.ts
@@ -2,21 +2,21 @@ import type { GetServerSideProps } from "next";
 import { getServerAuthSession } from "./get-server-auth-session";
 
 export const protectedRouterPage: GetServerSideProps = async (context) => {
-    const { req, res } = context;
-    const session = await getServerAuthSession({ req, res });
+  const { req, res } = context;
+  const session = await getServerAuthSession({ req, res });
 
-    const id = context.query?.id;
-  
-    if (!session) {
-      return {
-        redirect: {
-          destination: "/",
-          permanent: false,
-        },
-      };
-    }
-  
+  const id = context.query?.id;
+
+  if (!session) {
     return {
-      props: { session, id },
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
     };
+  }
+
+  return {
+    props: { session, id },
   };
+};
