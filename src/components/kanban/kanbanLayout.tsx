ce8155a1dc476034a382e1e8658417eb4931bce4
ElliotLuque <elliotluque@gmail.com>
2022-12-18 03:57:37 +0100

    lint kanban layout component

--- src/components/kanban/kanbanLayout.tsx
+++ src/components/kanban/kanbanLayout.tsx
@@ -2,17 +2,20 @@ import Header from "../common/header";
 import Sidebar from "./sidebar";
 import type { ReactElement } from "react";
 
-const KanbanLayout: React.FC<{children: ReactElement, name: string, description: string}> = ({ children, name, description }) => {
-    return <div className="flex h-screen flex-col">
+const KanbanLayout: React.FC<{
+  children: ReactElement;
+  name: string;
+  description: string;
+}> = ({ children, name, description }) => {
+  return (
+    <div className="flex h-screen flex-col">
       <Header />
       <main className="flex h-full flex-row">
-        <Sidebar
-          name={name}
-          description={description}
-        />
+        <Sidebar name={name} description={description} />
         {children}
       </main>
     </div>
-}
+  );
+};
 
 export default KanbanLayout;
