7e67c40eac5bcbcf461d3080cd7d04f7b3f593aa
ElliotLuque <elliotluque@gmail.com>
2022-12-18 04:26:56 +0100

    extract user avatar from header

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,24 +1,5 @@
-import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
-import Image from "next/image";
-
-const UserAvatar: React.FC = () => {
-  const { data: session } = useSession();
-
-  return (
-    <Image
-      onClick={() => signOut()}
-      className="cursor-pointer rounded-full p-1"
-      width={42}
-      height={42}
-      src={
-        session?.user?.image ??
-        "https://t3.ftcdn.net/jpg/03/58/90/78/360_F_358907879_Vdu96gF4XVhjCZxN2kCG0THTsSQi8IhT.jpg"
-      }
-      alt="User image"
-    />
-  );
-};
+import UserAvatar from "./userAvatar";
 
 const Header: React.FC = () => {
   return (
@@ -27,14 +8,14 @@ const Header: React.FC = () => {
         <Link href="/dashboard" className="text-2xl font-bold text-indigo-500">
           Logo
         </Link>
-        <h1 className="text-lg">Projects</h1>
-        <h1 className="text-lg">My tasks</h1>
+        <h1 className="text-lg">Boards</h1>
+        <h1 className="text-lg">Tasks</h1>
       </div>
       <div className="flex flex-row items-center gap-4">
         <h1 className="text-lg">Search</h1>
         <h1 className="text-lg">Notification</h1>
         <h1 className="text-lg">Configuration</h1>
-        <UserAvatar />
+        <UserAvatar width={42} height={42} />
       </div>
     </header>
   );
---
+++ src/components/common/userAvatar.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import { signOut, useSession } from "next-auth/react";
+
+const UserAvatar: React.FC<{ width: number; height: number }> = ({
+  width,
+  height,
+}) => {
+  const { data: session } = useSession();
+
+  return (
+    <Image
+      onClick={() => signOut()}
+      className="cursor-pointer rounded-full p-1"
+      width={width}
+      height={height}
+      src={
+        session?.user?.image ??
+        "https://t3.ftcdn.net/jpg/03/58/90/78/360_F_358907879_Vdu96gF4XVhjCZxN2kCG0THTsSQi8IhT.jpg"
+      }
+      alt="User image"
+    />
+  );
+};
+
+export default UserAvatar;
