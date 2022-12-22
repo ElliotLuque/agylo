0c2cde708b546703bd70e2101097a722b467558c
ElliotLuque <elliotluque@gmail.com>
2022-12-22 02:25:42 +0100

    add create board icon to sidebar

--- src/components/common/sidebar.tsx
+++ src/components/common/sidebar.tsx
@@ -3,40 +3,51 @@ import { trpc } from "../../utils/trpc";
 import HomeIcon from "./svg/homeIcon";
 import TasksIcon from "./svg/tasksIcon";
 import NotificationIcon from "./svg/notificationIcon";
+import AddIcon from "./svg/addIcon";
+import { useState } from "react";
+import BoardCreateDialog from "../home/createDialog";
 
 const Sidebar: React.FC = () => {
   const { data: userBoards } = trpc.board.listBoards.useQuery();
 
+  const [openCreateDialog, setOpenCreateDialog] = useState(false);
+
   return (
     <aside className="border-r-1 fixed top-0 h-screen w-72 border border-gray-200 bg-white">
+      <BoardCreateDialog open={openCreateDialog} setOpen={setOpenCreateDialog} />
       <div className="flex h-full flex-col gap-2 py-4 px-7">
         <h1 className="mt-5 text-xl font-bold text-indigo-500">Logo agylo</h1>
-        <div className="mt-12 flex flex-col justify-center gap-6 text-lg font-semibold text-gray-800 ">
+        <div className="mt-10 flex flex-col justify-center gap-6 text-lg font-semibold text-gray-800">
           <Link href="/dashboard">
-            <div className="flex flex-row items-center gap-3">
+            <div className="flex items-center gap-3">
               <HomeIcon />
               <h1>Home</h1>
             </div>
           </Link>
           <Link href="/my-tasks">
-            <div className="flex flex-row items-center gap-3">
+            <div className="flex items-center gap-3">
               <TasksIcon />
               <h1>My tasks</h1>
             </div>
           </Link>
-          <h1>
-            <div className="flex flex-row items-center gap-3">
+          <Link href="/inbox">
+            <div className="flex items-center gap-3">
               <NotificationIcon />
               <h1>Inbox</h1>
             </div>
-          </h1>
+          </Link>
         </div>
         <div className="mt-8">
-          <h1 className="font-medium text-md text-stone-500">Boards</h1>
-          <div className="ml-1.5 mt-5 mb-4 flex h-76 flex-col gap-3.5 overflow-auto">
+          <div className="flex w-full items-center justify-between">
+            <h1 className="text-lg font-medium text-stone-500">Boards</h1>
+            <div onClick={() => setOpenCreateDialog(true)}>
+              <AddIcon />
+            </div>
+          </div>
+          <div className="h-76 ml-1.5 mt-5 mb-4 flex flex-col gap-3.5 overflow-auto">
             {userBoards?.map((board) => (
               <Link
-                className="font-bold text-sm text-gray-800"
+                className="text-sm font-bold text-gray-800"
                 href={`/board/${board.boardId}`}
                 key={board.boardId}
               >
---
+++ src/components/common/svg/addIcon.tsx
@@ -0,0 +1,20 @@
+const AddIcon: React.FC = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.9}
+      stroke="currentColor"
+      className="h-5 w-5 cursor-pointer"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 4.5v15m7.5-7.5h-15"
+      />
+    </svg>
+  );
+};
+
+export default AddIcon;
