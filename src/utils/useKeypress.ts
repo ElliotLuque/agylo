85d3f65f18519ea7e6a1a01214c29ba3aa34dd08
ElliotLuque <elliotluque@gmail.com>
2023-01-06 16:25:28 +0100

    add hook for key presses

---
+++ src/utils/useKeypress.ts
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+
+export const useKeypress = (key: string, callback: () => void) => {
+  useEffect(() => {
+    const handle = (e: KeyboardEvent) => {
+      if (e.key === key) {
+        callback();
+      }
+    };
+    document.addEventListener("keyup", handle);
+    return () => document.removeEventListener("keyup", handle);
+  }, [key, callback]);
+};
