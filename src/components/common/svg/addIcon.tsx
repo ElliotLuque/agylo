faa562f2f873320d98305e82d18292106a8860db
ElliotLuque <elliotluque@gmail.com>
2023-01-06 16:22:15 +0100

    add props to icon

--- src/components/common/svg/addIcon.tsx
+++ src/components/common/svg/addIcon.tsx
@@ -1,4 +1,4 @@
-const AddIcon: React.FC = () => {
+const AddIcon: React.FC<{classNames: string}> = ({classNames}) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -6,7 +6,7 @@ const AddIcon: React.FC = () => {
       viewBox="0 0 24 24"
       strokeWidth={1.9}
       stroke="currentColor"
-      className="h-5 w-5 cursor-pointer"
+      className={classNames}
     >
       <path
         strokeLinecap="round"
