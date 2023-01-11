5dcb057722b43344b17e031b08da4ec20920750b
ElliotLuque <elliotluque@gmail.com>
2023-01-11 17:22:31 +0100

    fix imports

--- src/components/project/views/kanban/task/taskSortable.tsx
+++ src/components/project/views/kanban/task/taskSortable.tsx
@@ -1,6 +1,6 @@
 import { useSortable } from '@dnd-kit/sortable'
 import { CSS } from '@dnd-kit/utilities'
-import type { Task } from '../../../../types/kanban'
+import type { Task } from '../../../../../types/kanban'
 
 const TaskSortable: React.FC<Task & { cursor: string }> = ({
   id,
