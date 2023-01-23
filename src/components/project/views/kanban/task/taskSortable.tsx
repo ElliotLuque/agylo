2fac883f916a3ced162bf9250dd6e703d236fe64
ElliotLuque <elliotluque@gmail.com>
2023-01-24 00:10:27 +0100

    apply styles to task sortable

--- src/components/project/views/kanban/task/taskSortable.tsx
+++ src/components/project/views/kanban/task/taskSortable.tsx
@@ -3,13 +3,25 @@ import { CSS } from '@dnd-kit/utilities'
 import type { Task } from '../../../../../types/kanban'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import UserAvatar from '../../../../misc/userAvatar'
+import {
+  ChatBubbleBottomCenterIcon,
+  PaperClipIcon,
+} from '@heroicons/react/24/outline'
+import { motion } from 'framer-motion'
+import LabelIcon from '../taskDetails/labels/labelIcon'
 
-const TaskSortable: React.FC<Task & { cursor: string, onClick: () => void }> = ({
-  id,
+const TaskSortable: React.FC<
+  Task & { cursor: string; onClick: () => void }
+> = ({
   title,
   taskKey,
   index,
-  cursor
+  assignee,
+  cursor,
+  labels,
+  attachmentCount,
+  commentCount,
 }) => {
   const {
     attributes,
@@ -32,11 +44,50 @@ const TaskSortable: React.FC<Task & { cursor: string, onClick: () => void }> = (
 
   if (isDragging) {
     return (
-      <div
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.3 }}
         ref={setNodeRef}
         style={style}
-        className='min-h-[7rem] w-[17rem] rounded-lg border-[0.115rem] border-dashed border-gray-300 bg-gray-50'
-      ></div>
+        className='min-h-[6rem] w-[18rem] rounded-lg border-[0.115rem] border-dashed border-gray-300 bg-gray-50 px-3.5 pt-2 pb-2.5 '
+      >
+        <div className='invisible flex flex-col gap-1'>
+          <div className='flex items-center gap-2'>
+            <p className='select-none truncate font-medium text-gray-700'>
+              {title}
+            </p>
+          </div>
+          <div className='flex items-center gap-2'>
+            {labels?.map((label, index) => (
+              <LabelIcon
+                key={index}
+                colorId={label.label.colorId}
+                name={label.label.name}
+                classNames='mt-2 mb-6 text-xs'
+              />
+            ))}
+          </div>
+        </div>
+
+        <div className='flex items-center justify-between'>
+          <UserAvatar width={25} height={25} imageUrl={null} isInvisible />
+          <div className='flex items-center gap-2 text-gray-900 opacity-70'>
+            {commentCount !== null && commentCount > 0 && (
+              <div className='flex items-center gap-1'>
+                <ChatBubbleBottomCenterIcon className='mt-0.5 h-3 w-3' />
+                <p className='text-sm'>{commentCount}</p>
+              </div>
+            )}
+            {attachmentCount !== null && attachmentCount > 0 && (
+              <div className='flex items-center gap-1'>
+                <PaperClipIcon className='mt-0.5 h-3 w-3' />
+                <p className='text-sm'>{attachmentCount}</p>
+              </div>
+            )}
+          </div>
+        </div>
+      </motion.div>
     )
   }
 
@@ -50,12 +101,48 @@ const TaskSortable: React.FC<Task & { cursor: string, onClick: () => void }> = (
       {...listeners}
       ref={setNodeRef}
       style={style}
-      className={`flex min-h-[7rem] w-[17rem] ${cursor} flex-col gap-2 rounded-lg border-[0.09rem] border-gray-200 bg-white p-4`}
+      className={`flex min-h-[6rem] w-[18rem] ${cursor} flex-col justify-between rounded-lg border-[0.09rem] border-gray-200 bg-white px-3.5 pt-2 pb-2.5 focus:outline-none`}
     >
-      <p className='select-none'>
-        {id}-{title} at index {index}{' '}
-        <span className='font-bold'>{taskKey}</span>
-      </p>
+      <div className='flex flex-col gap-1'>
+        <div className='flex items-center gap-2'>
+          <p className='select-none truncate font-medium text-gray-700'>
+            {title}
+          </p>
+        </div>
+        <div className='flex items-center gap-2'>
+          {labels?.map((label, index) => (
+            <LabelIcon
+              key={index}
+              colorId={label.label.colorId}
+              name={label.label.name}
+              classNames='mt-2 mb-6 text-xs'
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className='flex items-center justify-between'>
+        <UserAvatar
+          width={25}
+          height={25}
+          imageUrl={assignee?.image as string}
+          isInvisible={false}
+        />
+        <div className='flex items-center gap-2 text-gray-900 opacity-70'>
+          {commentCount !== null && commentCount > 0 && (
+            <div className='flex items-center gap-1'>
+              <ChatBubbleBottomCenterIcon className='mt-0.5 h-3 w-3' />
+              <p className='text-sm'>{commentCount}</p>
+            </div>
+          )}
+          {attachmentCount !== null && attachmentCount > 0 && (
+            <div className='flex items-center gap-1'>
+              <PaperClipIcon className='mt-0.5 h-3 w-3' />
+              <p className='text-sm'>{attachmentCount}</p>
+            </div>
+          )}
+        </div>
+      </div>
     </Link>
   )
 }
