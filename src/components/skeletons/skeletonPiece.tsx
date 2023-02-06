48422e809275394a4ede96f5bc85b51547f75e38
ElliotLuque <elliotluque@gmail.com>
2023-02-06 17:14:30 +0100

    add skeleton piece to build skeleton loaders

--- src/components/skeletons/skeletonPiece.tsx
+++ src/components/skeletons/skeletonPiece.tsx
@@ -1,8 +1,19 @@
-const SkeletonPiece: React.FC<{ classNames?: string }> = ({ classNames }) => {
+import type { ReactElement } from 'react'
+
+const SkeletonPiece: React.FC<{
+	classNames?: string
+	children?: ReactElement
+	darker?: boolean
+	lighter?: boolean
+}> = ({ classNames, children, darker, lighter }) => {
 	return (
 		<span
-			className={`${classNames} inline-flex animate-pulse bg-slate-200 dark:bg-slate-700`}
-		/>
+			className={`${classNames} ${darker ? 'bg-gray-300' : 'bg-slate-200'} ${
+				lighter ? 'bg-slate-100' : 'bg-slate-200'
+			} inline-flex animate-pulse duration-75 dark:bg-slate-700`}
+		>
+			{children}
+		</span>
 	)
 }
 
